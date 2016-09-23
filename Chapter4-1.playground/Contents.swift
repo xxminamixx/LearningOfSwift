24a26630fc9f2e296f58f4f04d41c43b01afc32a
南　京兵 <k_minami@waku-2.com>
2016-09-23 14:11:49 +0900

    Chapter4-1Objective-Cにおけるnil

---
+++ Chapter4-1.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// NSNotFoundによる値がなかった場合の処理
+// 戻り値がnilの場合NSIntergerはプリミティブ型なので0が返ってくる
+// これを想定していないと致命的なエラーとなる
+//NSArray *ary = [self maybeReturnArray];
+//NSUInteger index = [ary indexOfObject:@"hello"];
+//if (index == NSNotFound) {
+//    [self notFoundTask];
+//}
